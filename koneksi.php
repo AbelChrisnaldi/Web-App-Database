cd404bcfd30bf710abcb62ad11efcba1bedb9c6f
AbelChrisnaldi <abelchris123@gmail.com>
2025-11-23 10:27:32 +0700

    second commit

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";          // sesuaikan
-$pass = "";              // sesuaikan
-$db   = "db_mahasiswa";  // nama database tadi
-
-$conn = new mysqli($host, $user, $pass, $db);
-
+$host = "dbmahasiswa1.mysql.database.azure.com";
+$user = "admin123@dbmahasiswa1";
+$pass = "Mahasiswa2025!";
+$db   = "db_mahasiswa";
+$conn = new mysqli($host, $user, $pass, $db, 3306);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
 ?>
